301f6024b299b09530354d0b42e418df50751b0d
WuQiaoq <qwu2324@politecnics.barcelona>
2025-06-04 11:49:04 +0200

    añadir más submarinos

---
+++ src/SeaCreature.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class SeaCreature {
+    private JLabel label;
+    private int x, y;
+    private int speed;
+
+    private Timer timer;
+    private boolean isGif;
+
+}
